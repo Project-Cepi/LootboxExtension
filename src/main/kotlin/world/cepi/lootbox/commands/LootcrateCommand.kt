a93ace07705ccb8e7f4b765806b105e682780449
CmdrNorthpaw <thetuxkid@gmail.com>
2021-01-26 21:53:37 +0000

    Loot crate command!

---
+++ src/main/kotlin/world/cepi/lootbox/commands/LootcrateCommand.kt
@@ -0,0 +1,83 @@
+package world.cepi.lootbox.commands
+
+import net.minestom.server.chat.ChatColor
+import net.minestom.server.chat.ColoredText
+import net.minestom.server.command.CommandSender
+import net.minestom.server.command.builder.Arguments
+import net.minestom.server.command.builder.Command
+import net.minestom.server.command.builder.arguments.ArgumentType
+import net.minestom.server.command.builder.arguments.ArgumentWord
+import net.minestom.server.command.builder.arguments.minecraft.ArgumentIntRange
+import net.minestom.server.command.builder.arguments.number.ArgumentInteger
+import net.minestom.server.data.DataImpl
+import net.minestom.server.entity.Player
+import net.minestom.server.item.ItemStack
+import net.minestom.server.item.Material
+import world.cepi.lootbox.LootboxExtension
+import world.cepi.lootbox.model.LootCrate
+
+class LootcrateCommand : Command("lootcrate") {
+    private val name: ArgumentWord = ArgumentType.Word("name")
+    private val chance: ArgumentInteger = ArgumentType.Integer("chance")
+
+    private val create = ArgumentType.Word("create").from("create")
+    private val add = ArgumentType.Word("create").from("add")
+    private val get = ArgumentType.Word("get").from("get")
+
+    init {
+
+        addSyntax({sender: CommandSender, args: Arguments ->
+            val crate = LootboxExtension.crates.firstOrNull { it.name == args.getString("name") }
+            if (crate != null) sender.sendMessage(ColoredText.of(ChatColor.RED, "That crate already exists!"))
+
+            LootboxExtension.crates.add(LootCrate(args.getString("name"), mutableListOf()))
+            sender.sendMessage(ColoredText.of(ChatColor.BRIGHT_GREEN, "Added crate!"))
+        }, create, name)
+
+        addSyntax({sender: CommandSender, args: Arguments ->
+            val name = args.getWord("name")
+            val crate = LootboxExtension.crates.firstOrNull { it.name == name }
+            if (crate == null) {
+                sender.sendMessage(ColoredText.of(ChatColor.RED, "That crate does not exist!"))
+                return@addSyntax
+            }
+            if (sender !is Player) {
+                sender.sendMessage(ColoredText.of(ChatColor.RED, "Please only run this command as a player"))
+                return@addSyntax
+            }
+
+            val stack = sender.itemInMainHand
+            crate.entries.add(LootCrate.LootCrateEntry(
+                namespace = stack.material.name,
+                count = stack.amount.toInt(),
+                chance = args.getInteger("chance").toFloat() / 100
+            ))
+            LootboxExtension.crates.removeIf { it.name == name }
+            LootboxExtension.crates.add(crate)
+
+            sender.sendMessage(ColoredText.of(ChatColor.BRIGHT_GREEN, "Added item to crate!"))
+        }, add, name, chance)
+
+        addSyntax({sender: CommandSender, args: Arguments ->
+            val name = args.getWord("name")
+            val crate = LootboxExtension.crates.firstOrNull { it.name == name }
+            if (crate == null) {
+                sender.sendMessage(ColoredText.of(ChatColor.RED, "That crate does not exist!"))
+                return@addSyntax
+            }
+            if (sender !is Player) {
+                sender.sendMessage(ColoredText.of(ChatColor.RED, "Please only run this command as a player"))
+                return@addSyntax
+            }
+
+            val barrel = ItemStack(Material.BARREL, 1)
+            val barrelData = DataImpl()
+            barrelData.set("loot", crate, LootCrate::class.java)
+            barrel.data = barrelData
+            barrel.displayName = ColoredText.of(ChatColor.GOLD, "Loot Crate")
+
+            sender.inventory.addItemStack(barrel)
+
+        }, get, name)
+    }
+}
