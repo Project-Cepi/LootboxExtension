61c8bb064083f8512ce939547d650d01df116304
CmdrNorthpaw <thetuxkid@gmail.com>
2021-01-26 21:18:39 +0000

    Load lootbox crates from JSON (no saving yet)

--- src/main/kotlin/world/cepi/lootbox/LootboxExtension.kt
+++ src/main/kotlin/world/cepi/lootbox/LootboxExtension.kt
@@ -1,6 +1,9 @@
 package world.cepi.lootbox
 
+import kotlinx.serialization.json.Json
 import net.minestom.server.extensions.Extension;
+import world.cepi.lootbox.model.LootCrate
+import java.io.File
 
 class LootboxExtension : Extension() {
 
@@ -10,6 +13,18 @@ class LootboxExtension : Extension() {
 
     override fun terminate() {
         logger.info("[ExampleExtension] has been disabled!")
+    }
+    companion object {
+        private val lootboxesFile = File("./lootboxes/")
+
+        val crates: Array<LootCrate>
+        get() {
+            val boxesList = mutableListOf<LootCrate>()
+            if (lootboxesFile.listFiles() == null) lootboxesFile.mkdirs()
+            lootboxesFile.listFiles()?.forEach { boxesList.add(Json.decodeFromString(LootCrate.serializer(), it.readText())) }
+
+            return boxesList.toTypedArray()
+        }
     }
 
 }
