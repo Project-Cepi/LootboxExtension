2d7dd4e08994b96ca05e9abcab2683e99a6e8c37
CmdrNorthpaw <30018896+CmdrNorthpaw@users.noreply.github.com>
2021-01-26 20:50:56 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin.
+    id("org.jetbrains.kotlin.jvm") version "1.4.21-2"
+    kotlin("plugin.serialization") version "1.4.21"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    maven
+
+    // Apply the application plugin to add support for building a jar
+    java
+    id("org.jetbrains.dokka") version "1.4.20"
+}
+
+repositories {
+    // Use jcenter for resolving dependencies.
+    jcenter()
+
+    // Use mavenCentral
+    maven(url = "https://repo1.maven.org/maven2/")
+    maven(url = "http://repo.spongepowered.org/maven")
+    maven(url = "https://libraries.minecraft.net")
+    maven(url = "https://jitpack.io")
+    maven(url = "https://jcenter.bintray.com/")
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    compileOnly(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    compileOnly(kotlin("stdlib"))
+
+    // Use the Kotlin reflect library.
+    compileOnly(kotlin("reflect"))
+
+    // Compile Minestom into project
+    compileOnly("com.github.Minestom:Minestom:-SNAPSHOT")
+
+    // OkHttp
+    compileOnly("com.squareup.okhttp3", "okhttp", "4.9.0")
+
+    // import kotlinx serialization
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks {
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+        archiveBaseName.set("example")
+        mergeServiceFiles()
+        minimize()
+
+    }
+
+    test { useJUnitPlatform() }
+
+    build { dependsOn(shadowJar) }
+
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
