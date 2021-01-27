b823ab8a1b7943127f437e7f0925281fd1aa92ef
CmdrNorthpaw <thetuxkid@gmail.com>
2021-01-27 10:01:35 +0000

    Add loot data to placed barrels (if it has them

---
+++ src/main/kotlin/world/cepi/lootbox/listeners/BlockPlaceListener.kt
@@ -0,0 +1,12 @@
+package world.cepi.lootbox.listeners
+
+import net.minestom.server.data.DataImpl
+import net.minestom.server.event.player.PlayerBlockPlaceEvent
+import net.minestom.server.instance.block.Block
+
+fun onBlockPlace(event: PlayerBlockPlaceEvent) {
+    val block = event.player.instance?.getBlock(event.blockPosition)
+    val data = event.player.itemInMainHand.data ?: DataImpl()
+
+    if (block == Block.BARREL && data.hasKey("loot")) event.blockData = data
+}
