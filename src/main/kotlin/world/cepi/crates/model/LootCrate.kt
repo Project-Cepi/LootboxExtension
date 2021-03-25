3340f6ca4cd602181eac714a50d5b375de0485c6
LeoDog896 <LeoDog896@hotmail.com>
2021-03-25 11:00:43 -0400

    Fix gradient italics

--- src/main/kotlin/world/cepi/crates/model/LootCrate.kt
+++ src/main/kotlin/world/cepi/crates/model/LootCrate.kt
@@ -3,6 +3,7 @@ package world.cepi.crates.model
 import kotlinx.serialization.Serializable
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
 import net.minestom.server.data.DataImpl
 import net.minestom.server.item.ItemStack
 import net.minestom.server.item.Material
@@ -20,10 +21,11 @@ data class LootCrate(
         val barrelData = DataImpl()
         barrelData.set(lootKey, this)
         barrel.data = barrelData
-        barrel.displayName = "<gradient:yellow:gold>Loot Crate".asMini()
+        barrel.displayName = "<gradient:yellow:gold>Loot Crate".asMini().decoration(TextDecoration.ITALIC, false)
         barrel.lore = arrayListOf<Component>(
             Component.space(),
-            Component.text("ID: ", NamedTextColor.GRAY).append(Component.text(name, NamedTextColor.WHITE))
+            Component.text("ID: ", NamedTextColor.GRAY).decoration(TextDecoration.ITALIC, false)
+                .append(Component.text(name, NamedTextColor.WHITE).decoration(TextDecoration.ITALIC, false))
         )
         return barrel
     }
