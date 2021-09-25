8a558bd2e5f5fb22503a3110016421f830a8e36b
LeoDog896 <LeoDog896@hotmail.com>
2021-09-25 10:37:44 -0400

    Add Teleport Reward

--- src/main/kotlin/world/cepi/crates/rewards/TeleportReward.kt
+++ src/main/kotlin/world/cepi/crates/rewards/TeleportReward.kt
@@ -7,17 +7,19 @@ import net.minestom.server.entity.Player
 import net.minestom.server.instance.Instance
 import net.minestom.server.utils.location.RelativeVec
 import world.cepi.crates.model.LootCrate
+import world.cepi.kstom.serializer.RelativeVecSerializer
 import world.cepi.kstom.util.asVec
 
-//@Serializable
-//class TeleportReward(
-//    val relativeVec: RelativeVec
-//): Reward() {
-//
-//    override fun dispatch(target: Player, lootcrate: LootCrate, instance: Instance, position: Point): Component {
-//        target.teleport(relativeVec.from(position.asVec().asPosition()).asPosition())
-//
-//        return generateComponent()
-//    }
-//
-//}
+@Serializable
+class TeleportReward(
+    @Serializable(with = RelativeVecSerializer::class)
+    val relativeVec: RelativeVec
+): Reward() {
+
+    override fun dispatch(target: Player, lootcrate: LootCrate, instance: Instance, position: Point): Component {
+        target.teleport(relativeVec.from(position.asVec().asPosition()).asPosition())
+
+        return generateComponent()
+    }
+
+}
